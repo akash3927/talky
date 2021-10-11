1463b39ccdcae54a86c96ec0e977cd1b9c21a90a
Akash <akashkothawade77@gmail.com>
2021-10-11 17:12:30 +0530

    socket basic part completed

---
+++ nodeServer/index.js
@@ -0,0 +1,17 @@
+/** @format */
+
+const io = require('socket.io')(8000);
+const users = {};
+
+io.on('connection', (socket) => {
+	socket.on('new-user-joined', (name) => {
+		users[socket.id] = name;
+		socket.broadcast.emit('user-joined', name);
+	});
+	socket.on('send', (message) => {
+		socket.broadcast.emit('reiceive', {
+			message: message,
+			name: user[socket.id],
+		});
+	});
+});
