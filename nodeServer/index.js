793ed9e76d3fb693c84bef0ce749f461d2a2b20c
Akash <akashkothawade77@gmail.com>
2021-10-12 15:53:24 +0530

    cors error fixed

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,17 +1,22 @@
 /** @format */
 
-const io = require('socket.io')(8000);
+const io = require('socket.io')(8000, {
+	cors: {
+		origin: 'http://127.0.0.1:5500',
+	},
+});
 const users = {};
 
 io.on('connection', (socket) => {
 	socket.on('new-user-joined', (name) => {
+		console.log('new user', name);
 		users[socket.id] = name;
 		socket.broadcast.emit('user-joined', name);
 	});
 	socket.on('send', (message) => {
 		socket.broadcast.emit('reiceive', {
 			message: message,
-			name: user[socket.id],
+			name: users[socket.id],
 		});
 	});
 });
