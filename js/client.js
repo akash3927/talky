518b6f202b2611a6b38cf4e68cb8f8745742eeba
Akash <akashkothawade77@gmail.com>
2021-10-12 16:46:39 +0530

    reiceive functinolity working

--- js/client.js
+++ js/client.js
@@ -1,10 +1,36 @@
 /** @format */
 
+// const { SocketAddress } = require('net');
+
 const socket = io('http://localhost:8000');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector('.container');
 
+const append = (message, position) => {
+	const messageElement = document.createElement('div');
+	messageElement.innerText = message;
+	messageElement.classList.add('message');
+	messageElement.classList.add(position);
+	messageContainer.append(messageElement);
+};
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const message = messageInput.value;
+	append(`you: ${message}`, 'right');
+	socket.emit('send', message);
+	messageInput.value = '';
+});
+
 const name = prompt('enter your name ');
 socket.emit('new-user-joined', name);
+
+socket.on('user-joined', (name) => {
+	append(`${name} joied the chat`, 'right');
+});
+
+socket.on('reiceive', (data) => {
+	append(`${data.name}: ${data.message}`, 'left');
+});
